313436733ee89d4b3880144c4ef8462f3d98884e
afdhali <afdhali.apreto@gmail.com>
2026-02-02 19:56:33 +0700

    shadcn install, sign in/ up page, header

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,8 @@
+import { ClerkProvider } from "@clerk/nextjs";
 import type { Metadata } from "next";
 import { Outfit } from "next/font/google";
 import "./globals.css";
-import {
-  ClerkProvider,
-  SignedIn,
-  SignedOut,
-  SignInButton,
-  SignUpButton,
-  UserButton,
-} from "@clerk/nextjs";
+import HeaderWrapper from "@/components/layout/header-wrapper";
 
 const outfitFont = Outfit({
   subsets: ["latin"],
@@ -30,19 +24,7 @@ export default function RootLayout({
     <ClerkProvider>
       <html lang="en">
         <body className={`${outfitFont.className} antialiased`}>
-          <header className="flex justify-end items-center p-4 gap-4 h-16">
-            <SignedOut>
-              <SignInButton />
-              <SignUpButton>
-                <button className="bg-[#6c47ff] text-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
-                  Sign Up
-                </button>
-              </SignUpButton>
-            </SignedOut>
-            <SignedIn>
-              <UserButton />
-            </SignedIn>
-          </header>
+          <HeaderWrapper />
           {children}
         </body>
       </html>
